4e95e594ce223736a85d7ace176897aed9b08217
隋国珍 <s15863549041@126.com>
2019-01-18 14:33:23 +0800

    更改格式

--- index.js
+++ index.js
@@ -13,7 +13,7 @@
         //类名
         className: "monster",
         //字体大小
-        fontSize:50
+        fontSize: 50
     };
     Loading.foundation = {
         loading: function () {
@@ -22,39 +22,40 @@
         wrapper: function () {
             return '<div class="' + this.config.className + '-loading-wrapper">'
         },
-        icon:function(){
-            return '<i class="'+this.config.className+'-loading-icon">'+this.config.icon+'</i>';
+        icon: function () {
+            return '<i class="' + this.config.className + '-loading-icon">' + this.config.icon + '</i>';
         },
-        text:function(){
-            return '<div class="'+this.config.className+'-loading-text">'+this.config.text+'</div>';
+        text: function () {
+            return '<div class="' + this.config.className + '-loading-text">' + this.config.text + '</div>';
         }
     };
     Loading.prototype = {
         constructor: Loading,
-        proxy:function(func){
-            let that=this;
-            return (function(){
-                return func.apply(that,arguments);
+        proxy: function (func) {
+            let that = this;
+            return (function () {
+                return func.apply(that, arguments);
             })()
         },
         init: function (config) {
-            this.config=$.extend({},Loading.config,config);
-            this.loading=null;
-            this.build();
+            this.config = $.extend({}, Loading.config, config);
+            this.loading = null;
+            return this;
         },
-        build:function(){
-            this.loading=$(this.proxy(Loading.foundation.loading)).css("fontSize",this.config.fontSize);
-            this.loading.append($(this.proxy(Loading.foundation.wrapper)).
-            append($(this.proxy(Loading.foundation.icon))).
-            append($(this.proxy(Loading.foundation.text))));
-            $(this.config.element).css("position","relative").append(this.loading);
+        build: function () {
+            if (!this.loading) {
+                this.loading = $(this.proxy(Loading.foundation.loading)).css("fontSize", this.config.fontSize);
+                this.loading.append($(this.proxy(Loading.foundation.wrapper)).append($(this.proxy(Loading.foundation.icon))).append($(this.proxy(Loading.foundation.text))));
+                $(this.config.element).css("position", "relative").append(this.loading);
+            }
         },
-        remove:function(){
-            if(this.loading){
+        remove: function () {
+            if (this.loading) {
                 this.loading.remove();
+                this.loading=null;
             }
         }
     };
     Loading.prototype.init.prototype = Loading.prototype;
-    w.monsterLoading=Loading;
+    w.monsterLoading = Loading;
 })(window);
